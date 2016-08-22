f9f2bc29d0f44edf192d54646760181b92da7c39
Robert-Jan Bruintjes <r.j.bruintjes@gmail.com>
2016-08-22 13:30:32 +0200

    Finalized some details on the testcase for RNN.

--- test/testRecurrentNeuralNetwork.py
+++ test/testRecurrentNeuralNetwork.py
@@ -18,12 +18,13 @@ class Test(unittest.TestCase):
 
     def testRecurrentNeuralNetwork(self):
         experiment_repetitions = 1;        
-        experiments_to_run = [(False,True,1,2,False,1000),(False,True,1,2,True,4000),(False,True,2,2,False,8000)];
+        experiments_to_run = [(True,1,False,4,2000),(True,1,True,4,6000),(True,2,False,4,8000)];
+        #experiments_to_run = [(True,1,True,4,6000),(True,2,False,4,8000)];
         
-        for e, (single_digit, lstm, layers, n_max_digits, decoder, repetitions) in enumerate(experiments_to_run):
+        for e, (lstm, layers, decoder, n_max_digits, repetitions) in enumerate(experiments_to_run):
             scores = [];
             for j in range(experiment_repetitions):
-                stats = self.runTest(single_digit,lstm,layers,decoder,n_max_digits,repetitions);
+                stats = self.runTest(lstm,layers,decoder,n_max_digits,repetitions);
                 scores.append(stats['score']);
                 print("Iteration %d: %.2f percent" % (j+1, stats['score'] * 100));
             mean_score = np.mean(scores);
@@ -31,7 +32,7 @@ class Test(unittest.TestCase):
             self.assertGreaterEqual(mean_score, 1.0, 
                                     "Experiment %d: mean score is not perfect: %.2f percent" % (e, mean_score * 100));
 
-    def runTest(self, single_digit=False, lstm=True, layers=1, decoder=False, n_max_digits=24, repetitions=3000):
+    def runTest(self, lstm=True, layers=1, decoder=False, n_max_digits=24, repetitions=3000):
         # Testcase settings
         parameters = arguments.defaults;
         parameters['train_batch_size'] = False;
@@ -42,49 +43,45 @@ class Test(unittest.TestCase):
         data_dim = 5;
         hidden_dim = 8;
         output_dim = 5;
-        single_digit = single_digit;
-        minibatch_size = 5;
+        minibatch_size = 6;
         
         # Model initialization
         model = RecurrentNeuralNetwork(data_dim, hidden_dim, output_dim, 
                                        minibatch_size,
-                                       single_digit=single_digit, lstm=lstm,
+                                       single_digit=False, lstm=lstm,
                                        decoder=decoder,
                                        n_max_digits=n_max_digits,
                                        layers=layers);
         
         # Data generation
-        training_data = np.array([np.array([[1.0,0.0,0.0,0.0,0.0],[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0]]),
-                                  np.array([[1.0,0.0,0.0,0.0,0.0],[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0]]),
-                                  np.array([[0.0,1.0,0.0,0.0,0.0],[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0]]),
-                                  np.array([[0.0,1.0,0.0,0.0,0.0],[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,0.0]]),
-                                  np.array([[0.0,1.0,0.0,0.0,0.0],[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,1.0,0.0,0.0]])]);
-        training_targets = np.array([np.array([1.0,0.0,0.0,0.0,0.0]),
-                                         np.array([0.0,1.0,0.0,0.0,0.0]),
-                                         np.array([0.0,0.0,1.0,0.0,0.0]),
-                                         np.array([0.0,0.0,0.0,1.0,0.0]),
-                                         np.array([0.0,0.0,0.0,1.0,0.0])]);
-        training_targets_multi_digit = np.array([np.array([[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),
-                                        np.array([[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),
-                                        np.array([[0.0,0.0,1.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),
-                                        np.array([[0.0,0.0,0.0,1.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),
-                                        np.array([[0.0,0.0,0.0,1.0,0.0],[0.0,0.0,0.0,0.0,1.0]])]);
-        training_labels = np.array([0,1,2,3,3]);
-        training_labels_multi_digit = np.array([[0],[1],[2],[3],[3]]);
+        training_data = np.array([np.array([[1.0,0.0,0.0,0.0,0.0],[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),   # 004 => 0244
+                                  np.array([[1.0,0.0,0.0,0.0,0.0],[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),   # 014 => 1344
+                                  np.array([[0.0,1.0,0.0,0.0,0.0],[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),   # 104 => 2044
+                                  np.array([[0.0,1.0,0.0,0.0,0.0],[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),   # 114 => 3144
+                                  np.array([[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0],[0.0,0.0,0.0,0.0,0.0]]),   # 04_ => 3014
+                                  np.array([[0.0,0.0,1.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0],[0.0,0.0,0.0,0.0,0.0]])]); # 24_ => 0314
+        training_targets_multi_digit = np.array([
+                                        np.array([[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,1.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0],[0.0,0.0,0.0,0.0,1.0]]),
+                                        np.array([[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,1.0,0.0],[0.0,0.0,0.0,0.0,1.0],[0.0,0.0,0.0,0.0,1.0]]),
+                                        np.array([[0.0,0.0,1.0,0.0,0.0],[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0],[0.0,0.0,0.0,0.0,1.0]]),
+                                        np.array([[0.0,0.0,0.0,1.0,0.0],[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0],[0.0,0.0,0.0,0.0,1.0]]),
+                                        np.array([[0.0,0.0,0.0,1.0,0.0],[1.0,0.0,0.0,0.0,0.0],[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]]),
+                                        np.array([[1.0,0.0,0.0,0.0,0.0],[0.0,0.0,0.0,1.0,0.0],[0.0,1.0,0.0,0.0,0.0],[0.0,0.0,0.0,0.0,1.0]])]);
+        training_labels_multi_digit = np.array([[0,2],[1,3],[2,0],[3,1],[3,0,1],[0,3,1]]);
         training_expressions = np.array(map(lambda e: "".join(map(lambda es: str(np.argmax(es)),e)), training_data))
         
-        if (single_digit):
-            dataset = FakeDataset(training_data,training_targets,training_labels,training_expressions,
-                                  training_data,training_targets,training_labels,training_expressions,
-                                  EOS_symbol_index=training_targets.shape[-1]);
-        else:
-            dataset = FakeDataset(training_data,training_targets_multi_digit,training_labels_multi_digit,training_expressions,
-                                  training_data,training_targets_multi_digit,training_labels_multi_digit,training_expressions, 
-                                  EOS_symbol_index=training_targets_multi_digit.shape[-1]);
+        dataset = FakeDataset(training_data,training_targets_multi_digit,training_labels_multi_digit,training_expressions,
+                              training_data,training_targets_multi_digit,training_labels_multi_digit,training_expressions, 
+                              EOS_symbol_index=training_targets_multi_digit.shape[-1]);
         
         start = time.clock();
         train(model, [dataset], parameters, 'testRNN', start, saveModels=False);
         stats = test(model, dataset, parameters, start);
+        
+        # Print sample predictions for every input sample
+        prediction, _ = model.predict(training_data);
+        for i, _ in enumerate(training_data):
+            print("Sample %d: %s" % (i,str(prediction[i])));
         
         return stats;
 
