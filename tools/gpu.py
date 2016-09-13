f263dc557e27abd72de26b969f4663ecd0d9a6f4
Robert-Jan Bruintjes <r.j.bruintjes@gmail.com>
2016-09-13 17:03:40 +0200

    Changed gpu check back to example provided by Theano in an attempt to find what is going run when using GPU.

--- tools/gpu.py
+++ tools/gpu.py
@@ -4,20 +4,25 @@ Created on 21 jun. 2016
 @author: Robert-Jan
 '''
 
-from theano import function, config, shared, sandbox
+from theano import function, config, shared
 import theano.tensor as T
 import numpy;
 
 def using_gpu():
     vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
-    #iters = 1000
+    iters = 1000
     
     rng = numpy.random.RandomState(22)
-    x = shared(numpy.asarray(rng.rand(vlen), config.floatX))  # @UndefinedVariable
+    x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
     f = function([], T.exp(x))
-    #for i in range(iters):
-    #    r = f()
-    return not numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]);
+    print(f.maker.fgraph.toposort())
+    for _ in range(iters):
+        r = f()
+    print("Result is %s" % (r,))
+    if numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
+        print('Used the cpu')
+    else:
+        print('Used the gpu')
 
 if __name__ == '__main__':
     print(using_gpu());
