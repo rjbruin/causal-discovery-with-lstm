2f333deb5468a82c89942c042f14364c8c4f38ba
Robert-Jan Bruintjes <r.j.bruintjes@gmail.com>
2016-11-04 16:47:29 +0100

    Added the autoencoder model, training script and experiment settings.

---
+++ autoencoder_train.py
@@ -0,0 +1,257 @@
+'''
+Created on 4 nov. 2016
+
+@author: Robert-Jan
+'''
+
+import time;
+import sys;
+from math import floor;
+
+from tools.file import save_to_pickle, load_from_pickle_with_filename;
+from tools.arguments import processCommandLineArguments;
+from tools.model import constructModels, set_up_statistics;
+from tools.gpu import using_gpu; # @UnresolvedImport
+
+from models.Autoencoder import Autoencoder;
+
+import numpy as np;
+import theano;
+import copy;
+from profiler import profiler
+
+def print_stats(precision, digit_precision):
+    # Print statistics
+    output = "\n";
+
+    # Print statistics
+    output += "Score: %.2f percent\n" % (precision);
+    output += "Digit-based score: %.2f percent\n" % (digit_precision);
+    
+    output += "\n";
+    print(output);
+
+def get_batch(isTrain, dataset, model, intervention_range, max_length, 
+              debug=False, base_offset=12):    
+    # Reseed the random generator to prevent generating identical batches
+    np.random.seed();
+    
+    if (isTrain):
+        storage = dataset.expressionsByPrefix;
+    else:
+        storage = dataset.testExpressionsByPrefix;
+    
+    batch = [];
+    subbatch_size = parameters['subbatch_size'];
+    while (len(batch) < model.minibatch_size):
+        interventionLocation = np.random.randint(max_length-intervention_range-base_offset, 
+                                                 max_length-base_offset);
+        
+        subbatch = [];
+        while (len(subbatch) < subbatch_size):
+            branch = storage.get_random_by_length(interventionLocation, getStructure=True);
+            
+            randomPrefix = np.random.randint(0,len(branch.fullExpressions));
+            subbatch.append((branch.fullExpressions[randomPrefix],
+                             branch.primedExpressions[randomPrefix]));
+        
+        # Add subbatch to batch
+        batch.extend(subbatch);
+    
+    data = [];
+    targets = [];
+    labels = [];
+    expressions = [];
+    for (expression, expression_prime) in batch:
+        data, _, _, expressions, _ = dataset.processor(";".join([expression, expression_prime]), 
+                                                                  data,targets, labels, expressions);
+    
+    data = dataset.fill_ndarray(data, 1, fixed_length=model.n_max_digits);
+    
+    return data, expressions;
+
+def test(model, dataset, parameters, max_length, base_offset, intervention_range, print_samples=False, 
+         sample_size=False, homogeneous=False, returnTestSamples=False):
+    # Test
+    print("Testing...");
+        
+    total = dataset.lengths[dataset.TEST];
+    printing_interval = 1000;
+    if (parameters['max_testing_size'] is not False):
+        total = parameters['max_testing_size'];
+        printing_interval = 100;
+    elif (sample_size != False):
+        total = sample_size;
+    
+    # Set up statistics
+    stats = set_up_statistics(dataset.data_dim, model.n_max_digits);
+    total_labels_used = {k: 0 for k in range(30)};
+    
+    # Predict
+    printed_samples = False;
+    totalError = 0.0;
+    k = 0;
+    testSamples = [];
+    precisions = [];
+    digit_precisions = [];
+    while k < total:
+        # Get data from batch
+        test_data, test_expressions = get_batch(False, dataset, model, 
+                                                intervention_range, 
+                                                max_length, debug=parameters['debug'],
+                                                base_offset=base_offset);
+        
+        predictions, precision, digit_precision, error = model.predict(test_data); 
+        precisions.append(precision);
+        digit_precisions.append(digit_precision);
+        totalError += error;
+        
+        if (parameters['only_cause_expression']):
+            prediction_1 = predictions;
+            predictions = [predictions];
+        else:
+            prediction_1 = predictions[0];
+            prediction_2 = predictions[1];
+        
+        for j in range(model.minibatch_size):
+            if (parameters['only_cause_expression'] is not False):
+                total_labels_used[test_expressions[j][0]] = True;
+            else:
+                total_labels_used[test_expressions[j][0]+";"+test_expressions[j][1]] = True;
+        
+        # Print samples
+        if (print_samples and not printed_samples):
+            for i in range(model.minibatch_size):
+                prefix = "# ";
+                if (parameters['only_cause_expression'] is not False):
+                    print(prefix + "Data      : %s" % "".join((map(lambda x: dataset.findSymbol[x], 
+                                                       np.argmax(test_data[i],len(test_data.shape)-2)))));
+                    print(prefix + "Prediction: %s" % "".join(map(lambda x: dataset.findSymbol[x], prediction_1[i])));
+                else:
+                    print(prefix + "Data       1: %s" % "".join((map(lambda x: dataset.findSymbol[x], 
+                                                       np.argmax(test_data[i,:,:model.data_dim/2],len(test_data.shape)-2)))));
+                    print(prefix + "Prediction 1: %s" % "".join(map(lambda x: dataset.findSymbol[x], prediction_1[i])));
+                    print(prefix + "Data       2: %s" % "".join((map(lambda x: dataset.findSymbol[x], 
+                                                       np.argmax(test_data[i,:,model.data_dim/2:],len(test_data.shape)-2)))));
+                    print(prefix + "Prediction 2: %s" % "".join(map(lambda x: dataset.findSymbol[x], prediction_2[i])));
+            printed_samples = True;
+
+        if (k % printing_interval == 0):
+            print("# %d / %d" % (stats['prediction_size'], total));
+        
+        k += model.minibatch_size;
+    
+    profiler.profile();
+    
+    print("Total testing error: %.2f" % totalError);
+    
+    print_stats(np.mean(precisions), np.mean(digit_precisions));
+    
+    if (returnTestSamples):
+        return stats, testSamples;
+    else:
+        return stats;
+
+if __name__ == '__main__':
+    theano.config.floatX = 'float32';
+    np.set_printoptions(precision=3, threshold=10000000);
+    profiler.off();
+    
+    # Specific settings - default name is time of experiment
+    name = time.strftime("%d-%m-%Y_%H-%M-%S");
+    saveModels = True;
+    
+    # Process parameters
+    parameters = processCommandLineArguments(sys.argv[1:]);
+    
+    # Warn for unusual parameters
+    if (parameters['max_training_size'] is not False):
+        print("WARNING! RUNNING WITH LIMIT ON TRAINING SIZE!");
+    if (not using_gpu()):
+        print("WARNING! RUNNING WITHOUT GPU USAGE!");
+    
+    # Check for valid subbatch size
+    if (parameters['minibatch_size'] % parameters['subbatch_size'] != 0):
+        raise ValueError("Subbatch size is not compatible with minibatch size: m.size = %d, s.size = %d" % 
+                            (parameters['minibatch_size'], parameters['subbatch_size']));
+    
+    # Check for valid intervention ranges
+    if (parameters['intervention_base_offset'] <= 0):
+        raise ValueError("Invalid intervention base offset: is %d, must be at least 1." % parameters['intervention_base_offset']);
+    
+    
+    
+    # Construct models
+    dataset, _ = constructModels(parameters, 0, {}, noModel=True);
+    actual_data_dim = dataset.data_dim;
+    if (parameters['only_cause_expression'] is False):
+        actual_data_dim *= 2;
+    model = Autoencoder(actual_data_dim, parameters['hidden_dim'], parameters['minibatch_size'], parameters['n_max_digits'], 
+                        parameters['learning_rate'], dataset.GO_symbol_index, dataset.EOS_symbol_index, parameters['only_cause_expression']);
+    
+    # Train on all datasets in succession
+    # Print settings headers to raw results file
+    print("# " + str(parameters));
+    
+    # Compute batching variables
+    repetition_size = dataset.lengths[dataset.TRAIN];
+    if (parameters['max_training_size'] is not False):
+        repetition_size = min(parameters['max_training_size'],repetition_size);
+    next_testing_threshold = parameters['test_interval'] * repetition_size;
+    
+    
+    
+    intervention_locations_train = {k: 0 for k in range(model.n_max_digits)};
+    for r in range(parameters['repetitions']):
+        stats = set_up_statistics(dataset.data_dim, model.n_max_digits);
+        total_error = 0.0;
+        # Print repetition progress and save to raw results file
+        print("Batch %d (repetition %d of %d, dataset 1 of 1) (samples processed after batch: %d)" % \
+                (r+1,r+1,parameters['repetitions'],(r+1)*repetition_size));
+        
+        # Train model per minibatch
+        k = 0;
+        printedProgress = -1;
+        while k < repetition_size:
+            profiler.start('train batch');
+            profiler.start('get train batch');
+            data, target_expressions = \
+                get_batch(True, dataset, model, 
+                          parameters['intervention_range'], model.n_max_digits, 
+                          debug=parameters['debug'],
+                          base_offset=parameters['intervention_base_offset']);
+            profiler.stop('get train batch');
+            
+            # Run training
+            profiler.start('train sgd');
+            error = model.sgd(data);
+            total_error += error;
+            profiler.stop('train sgd');
+            
+            # Print batch progress
+            if ((k+parameters['minibatch_size']) % (parameters['minibatch_size']*4) < parameters['minibatch_size'] and \
+                (k+parameters['minibatch_size']) / (parameters['minibatch_size']*4) > printedProgress):
+                printedProgress = (k+parameters['minibatch_size']) / (parameters['minibatch_size']*4);
+                print("# %d / %d (error = %.2f)" % (k+parameters['minibatch_size'], repetition_size, total_error));
+            
+            profiler.stop('train batch');
+            
+            k += parameters['minibatch_size'];
+        
+        # Report on error
+        print("Total error: %.2f" % total_error);
+        
+        # Intermediate testing if this was not the last iteration of training
+        # and we have passed the testing threshold
+        #if (r != repetition_size-1):
+        test(model, dataset, parameters, model.n_max_digits, parameters['intervention_base_offset'], parameters['intervention_range'], print_samples=parameters['debug'], 
+             sample_size=parameters['sample_testing_size'], homogeneous=parameters['homogeneous']);
+        
+        # Save weights to pickles
+        if (saveModels):
+            saveVars = model.getVars();
+            save_to_pickle('saved_models/%s_%d.model' % (name, r), saveVars, settings=parameters);
+    
+    print("Training finished!");
+    
+    
---
+++ models/Autoencoder.py
@@ -0,0 +1,161 @@
+'''
+Created on 4 nov. 2016
+
+@author: Robert-Jan
+'''
+
+import numpy as np;
+import theano;
+import theano.tensor as T;
+import lasagne;
+
+class Autoencoder(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, data_dim, hidden_dim, minibatch_size, n_max_digits, 
+                 learning_rate, GO_symbol_index, EOS_symbol_index, only_cause_expression):
+        '''
+        Constructor
+        '''
+        self.data_dim = data_dim;
+        self.minibatch_size = minibatch_size;
+        self.EOS_symbol_index = EOS_symbol_index;
+        self.n_max_digits = n_max_digits;
+        self.only_cause_expression = only_cause_expression;
+        
+        self.vars = {};
+        self.vars['hWf'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,hidden_dim)).astype('float32'), 'hWf');
+        self.vars['XWf'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim,hidden_dim)).astype('float32'), 'XWf')
+        self.vars['hWi'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,hidden_dim)).astype('float32'), 'hWi')
+        self.vars['XWi'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim,hidden_dim)).astype('float32'), 'XWi')
+        self.vars['hWc'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,hidden_dim)).astype('float32'), 'hWc')
+        self.vars['XWc'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim,hidden_dim)).astype('float32'), 'XWc')
+        self.vars['hWo'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,hidden_dim)).astype('float32'), 'hWo')
+        self.vars['XWo'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim,hidden_dim)).astype('float32'), 'XWo')
+        encode_params = [self.vars['hWf'], self.vars['XWf'], self.vars['hWi'], self.vars['XWi'], self.vars['hWc'], self.vars['XWc'], self.vars['hWo'], self.vars['XWo']];
+        
+        self.vars['DhWf'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,hidden_dim)).astype('float32'), 'DhWf')
+        self.vars['DXWf'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim,hidden_dim)).astype('float32'), 'DXWf')
+        self.vars['DhWi'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,hidden_dim)).astype('float32'), 'DhWi')
+        self.vars['DXWi'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim,hidden_dim)).astype('float32'), 'DXWi')
+        self.vars['DhWc'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,hidden_dim)).astype('float32'), 'DhWc')
+        self.vars['DXWc'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim,hidden_dim)).astype('float32'), 'DXWc')
+        self.vars['DhWo'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,hidden_dim)).astype('float32'), 'DhWo')
+        self.vars['DXWo'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim,hidden_dim)).astype('float32'), 'DXWo')
+        self.vars['DhWY'] = theano.shared(np.random.uniform(-np.sqrt(1.0/hidden_dim),np.sqrt(1.0/hidden_dim),(hidden_dim,data_dim)).astype('float32'), 'DhWY')
+        self.vars['DhbY'] = theano.shared(np.random.uniform(-np.sqrt(1.0/data_dim),np.sqrt(1.0/data_dim),(data_dim)).astype('float32'), 'DhbY')
+        decode_params = [self.vars['DhWf'], self.vars['DXWf'], self.vars['DhWi'], self.vars['DXWi'], self.vars['DhWc'], self.vars['DXWc'], self.vars['DhWo'], self.vars['DXWo'], self.vars['DhWY'], self.vars['DhbY']];
+        
+        data = T.ftensor3('data');
+        
+        hiddens_encode, _ = theano.scan(fn=self.lstm_predict_single_no_output,
+                                        sequences=data,
+                                        # Input a zero hidden layer
+                                        outputs_info=({'initial': T.zeros((minibatch_size, hidden_dim), dtype=theano.config.floatX), 'taps': [-1]}),
+                                        non_sequences=encode_params,
+                                        name='encode_scan');
+        
+        first_decode_x = T.zeros((minibatch_size, data_dim), dtype=theano.config.floatX);
+        T.set_subtensor(first_decode_x[:,GO_symbol_index],T.ones((minibatch_size), dtype=theano.config.floatX));
+        (output, _), _ = theano.scan(fn=self.lstm_predict_single,
+                                     # Input a zero hidden layer
+                                     outputs_info=({'initial': first_decode_x, 'taps': [-1]}, {'initial': hiddens_encode[-1], 'taps': [-1]}),
+                                     non_sequences=decode_params,
+                                     name='decode_scan',
+                                     n_steps=n_max_digits);
+        
+#         error = T.mean(T.nnet.categorical_crossentropy(output, data));
+        
+        cat_cross = -T.sum(data * T.log(output), axis=output.ndim-1);
+        mean_cross_per_sample = T.sum(cat_cross, axis=0) / float(self.n_max_digits);
+        error = T.mean(mean_cross_per_sample);
+        
+        predictions = T.argmax(output, axis=2);
+        digits_correct = T.eq(predictions, T.argmax(data, axis=2));
+        
+        # SGD
+        self.var_list = encode_params + decode_params;
+        updates = lasagne.updates.nesterov_momentum(error,self.var_list,learning_rate=learning_rate).items();
+        
+        # Defining functions
+        self._sgd = theano.function([data], [error, digits_correct],
+                                    updates=updates);
+        self._predict = theano.function([data], [output, predictions, digits_correct, error]);
+        self._encode = theano.function([data], [hiddens_encode[-1]]);
+        
+    def lstm_predict_single(self, previous_output, previous_hidden, hWf, XWf, hWi, XWi, hWc, XWc, hWo, XWo, hWY, hbY):
+        forget_gate = T.nnet.sigmoid(previous_hidden.dot(hWf) + previous_output.dot(XWf));
+        input_gate = T.nnet.sigmoid(previous_hidden.dot(hWi) + previous_output.dot(XWi));
+        candidate_cell = T.tanh(previous_hidden.dot(hWc) + previous_output.dot(XWc));
+        cell = forget_gate * previous_hidden + input_gate * candidate_cell;
+        output_gate = T.nnet.sigmoid(previous_hidden.dot(hWo) + previous_output.dot(XWo));
+        hidden = output_gate * cell;
+        
+        Y_output = T.nnet.softmax(hidden.dot(hWY) + hbY);
+        
+        return Y_output, hidden;
+    
+    def lstm_predict_single_no_output(self, current_X, previous_hidden, hWf, XWf, hWi, XWi, hWc, XWc, hWo, XWo):
+        forget_gate = T.nnet.sigmoid(previous_hidden.dot(hWf) + current_X.dot(XWf));
+        input_gate = T.nnet.sigmoid(previous_hidden.dot(hWi) + current_X.dot(XWi));
+        candidate_cell = T.tanh(previous_hidden.dot(hWc) + current_X.dot(XWc));
+        cell = forget_gate * previous_hidden + input_gate * candidate_cell;
+        output_gate = T.nnet.sigmoid(previous_hidden.dot(hWo) + current_X.dot(XWo));
+        hidden = output_gate * cell;
+        
+        return hidden;
+    
+    def sgd(self, data):
+        data = np.swapaxes(data, 0, 1);
+#         error, _, updates = self._sgd(data);
+#         print(str(updates));
+        error, _ = self._sgd(data);
+        
+        return error;
+    
+    def predict(self, data):
+        swapped_data = np.swapaxes(data, 0, 1);
+        output, predictions, digits_correct, error = self._predict(swapped_data);
+        output = np.swapaxes(output, 0, 1);
+        digits_correct = np.swapaxes(digits_correct, 0, 1);
+        
+        if (self.only_cause_expression is False):
+            predictions = [np.argmax(output[:,:,:self.data_dim/2], axis=2),
+                           np.argmax(output[:,:,self.data_dim/2:], axis=2)];
+            digits_correct_1 = np.equal(np.argmax(data[:,:,:self.data_dim/2], axis=2),predictions[0]);
+            digits_correct_2 = np.equal(np.argmax(data[:,:,self.data_dim/2:], axis=2),predictions[1]);
+            precision_1, digit_precision_1 = self.precision_from_digits_correct(data, digits_correct_1);
+            precision_2, digit_precision_2 = self.precision_from_digits_correct(data, digits_correct_2);
+            return predictions, (precision_1 + precision_2) / 2., (digit_precision_1 + digit_precision_2) / 2., error;
+        else:
+            precision, digit_precision = self.precision_from_digits_correct(data, digits_correct);
+            return predictions, precision, digit_precision, error;
+    
+    def encode(self, data):
+        data = np.swapaxes(data, 0, 1);
+        code = self._encode(data);
+        code = np.swapaxes(code, 0, 1);
+        return code;
+    
+    def precision_from_digits_correct(self, data, digits_correct):
+        correct = 0;
+        d_correct = 0;
+        digits_total = 0;
+        for i in range(data.shape[0]):
+            for j in range(data[i].shape[0]):
+                if (digits_correct[i,j] == 0.):
+                    # Stop if we encounter a wrong digit
+                    break;
+                if (np.argmax(data[i,j]) == self.EOS_symbol_index):
+                    # Stop if we encounter EOS and count as correct
+                    correct += 1;
+                    break;
+                d_correct += 1;
+            digits_total += j+1;
+        return correct / float(i+1), d_correct / float(digits_total);
+    
+    def getVars(self):
+        return self.vars.items();
