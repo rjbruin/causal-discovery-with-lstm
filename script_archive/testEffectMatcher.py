b9751f4e355943c0cda543bb8d6177ec6f5584fc
Robert-Jan Bruintjes <r.j.bruintjes@gmail.com>
2017-01-02 11:08:23 +0100

    Changed effect matcher to test s2mb2.

--- script_archive/testEffectMatcher.py
+++ script_archive/testEffectMatcher.py
@@ -8,7 +8,7 @@ from models.GeneratedExpressionDataset import GeneratedExpressionDataset;
 
 def testEffectMatcher():
     print("Preloading data...");
-    dataset = GeneratedExpressionDataset('./data/seq2ndmarkov_both_fixed/all.txt', './data/seq2ndmarkov_both_fixed/test.txt', './data/seq2ndmarkov_both_fixed/config.json',
+    dataset = GeneratedExpressionDataset('../data/seq2ndmarkov_both_2/all.txt', '../data/seq2ndmarkov_both_2/test.txt', '../data/seq2ndmarkov_both_2/config.json',
                                          operators=2,
                                          digits=8,
                                          dataset_type=GeneratedExpressionDataset.DATASET_SEQ2NDMARKOV,
@@ -23,7 +23,7 @@ def testEffectMatcher():
         top = dataset.strToIndices(dataset.expressionsByPrefix.expressions[i]);
         bot = dataset.strToIndices(dataset.expressionsByPrefix.primedExpressions[i]);
         
-        test = dataset.effect_matcher_seq2ndmarkov_both(top, bot, 8, 2, True);
+        test = dataset.effect_matcher_seq2ndmarkov_both_2(top, bot, 8, 2, True);
         if (not test):
             print("(effect) %d is wrong: top = %s, bot = %s" % (i, dataset.expressionsByPrefix.expressions[i], dataset.expressionsByPrefix.primedExpressions[i]));
 
