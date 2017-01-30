Fixed bug in subsystems_finish where an existing index was used for a loop within another loop, making it so that testing was not using any samples and therefore not scoring any precision above zero. Added better digit-based scoring to all experiments, aimed specifically at f-subs-dp.

models/RecurrentModel.py
@@ -50,17 +50,24 @@ class RecurrentModel(object):
         stats['prediction_histogram'] = RecurrentModel.addDicts(stats['prediction_1_histogram'], stats['prediction_2_histogram']);
 #         stats['prediction_size_histogram'] = RecurrentModel.addDicts(stats['prediction_1_size_histogram'], stats['prediction_2_size_histogram']);
         
-        stats['digit_correct'] = stats['digit_1_correct'] + stats['digit_2_correct'];
-        stats['digit_prediction_size'] = stats['digit_1_prediction_size'] + stats['digit_2_prediction_size'];
+        for i in range(20):
+            if (stats['digit_1_prediction_size'][i] > 0):
+                stats['digit_1_score'][i] = stats['digit_1_correct'][i] / float(stats['digit_1_prediction_size'][i]);
+            else:
+                stats['digit_1_score'][i] = 0.0;
+            if (stats['digit_2_prediction_size'][i] > 0):
+                stats['digit_2_score'][i] = stats['digit_2_correct'][i] / float(stats['digit_2_prediction_size'][i]);
+            else:
+                stats['digit_2_score'][i] = 0.0;
         
-        if (stats['digit_1_prediction_size'] > 0):
-            stats['digit_1_score'] = stats['digit_1_correct'] / float(stats['digit_1_prediction_size']);
-        else:
-            stats['digit_1_score'] = 0.0;
-        if (stats['digit_2_prediction_size'] > 0):
-            stats['digit_2_score'] = stats['digit_2_correct'] / float(stats['digit_2_prediction_size']);
-        else:
-            stats['digit_2_score'] = 0.0;
+        # Using fixed values!
+        dp_length = 20 - 8;
+        
+        stats['digit_1_total_score'] = np.mean([stats['digit_1_score'][i] for i in range(dp_length) if stats['digit_1_prediction_size'][i] > 0]);
+        stats['digit_2_total_score'] = np.mean([stats['digit_2_score'][i] for i in range(dp_length) if stats['digit_2_prediction_size'][i] > 0]);
+        stats['digit_correct'] = (stats['digit_1_total_score'] + stats['digit_2_total_score']) / 2.;
+        stats['digit_prediction_size'] = np.sum([stats['digit_1_prediction_size'][i] for i in range(dp_length)]) + np.sum([stats['digit_2_prediction_size'][i] for i in range(dp_length)])
+        
         
         if (stats['prediction_size'] > 0):
             stats['score'] = stats['correct'] / float(stats['prediction_size']);

subsystems_finish.py
@@ -35,6 +35,7 @@ def print_stats(stats, parameters, prefix=''):
 
     # Print statistics
     output += prefix + "Score: %.2f percent\n" % (stats['score']*100);
+    output += prefix + "Digit-based score: %.2f percent\n" % (stats['digit_score']*100);
     
     if (not parameters['only_cause_expression']):
         if (parameters['dataset_type'] != 3):
@@ -62,17 +63,24 @@ def print_stats(stats, parameters, prefix=''):
             output += prefix + "In dataset: %.2f percent\n" % (stats['inDatasetScore']*100);
 
     if (not parameters['only_cause_expression']):
-        output += prefix + "Digit-based (1) score: %.2f percent\n" % (stats['digit_1_score']*100);
+        output += prefix + "Digit-based (1) score: %.2f percent\n" % (stats['digit_1_total_score']*100);
+        output += prefix + "Digit-based (1) individual scores histogram: %s percent\n" % (str(stats['digit_1_score']));
+        output += prefix + "Digit prediction (1) histogram:   %s\n" % (str(stats['prediction_1_histogram']));
+        
+        output += prefix + "Digit-based (2) score: %.2f percent\n" % (stats['digit_2_total_score']*100);
+        output += prefix + "Digit-based (2) individual scores histogram: %s percent\n" % (str(stats['digit_2_score']));
+        output += prefix + "Digit prediction (2) histogram:   %s\n" % (str(stats['prediction_2_histogram']));
+        
         if (parameters['dataset_type'] != 3):
             output += prefix + "Prediction size (1) histogram:   %s\n" % (str(stats['prediction_1_size_histogram']));
-        output += prefix + "Digit (1) histogram:   %s\n" % (str(stats['prediction_1_histogram']));
-        
-        output += prefix + "Digit-based (2) score: %.2f percent\n" % (stats['digit_2_score']*100);
-        if (parameters['dataset_type'] != 3):
             output += prefix + "Prediction size (2) histogram:   %s\n" % (str(stats['prediction_2_size_histogram']));
-        output += prefix + "Digit (2) histogram:   %s\n" % (str(stats['prediction_2_histogram']));
+        else:
+            dp_length = 20 - 8;
+            output += prefix + "Digit-based score (1st quarter):    %.2f percent\n" % (np.mean([stats['digit_1_score'][i]*50. + stats['digit_2_score'][i]*50. for i in range(int((0./4)*dp_length),int((1./4)*dp_length))]))
+            output += prefix + "Digit-based score (2nd quarter):    %.2f percent\n" % (np.mean([stats['digit_1_score'][i]*50. + stats['digit_2_score'][i]*50. for i in range(int((1./4)*dp_length),int((2./4)*dp_length))]))
+            output += prefix + "Digit-based score (3rd quarter):    %.2f percent\n" % (np.mean([stats['digit_1_score'][i]*50. + stats['digit_2_score'][i]*50. for i in range(int((2./4)*dp_length),int((3./4)*dp_length))]))
+            output += prefix + "Digit-based score (4th quarter):    %.2f percent\n" % (np.mean([stats['digit_1_score'][i]*50. + stats['digit_2_score'][i]*50. for i in range(int((3./4)*dp_length),int((4./4)*dp_length))]))
         
-    output += prefix + "Digit-based score: %.2f percent\n" % (stats['digit_score']*100);
 #     output += prefix + "Prediction size histogram:   %s\n" % (str(stats['prediction_size_histogram']));
     output += prefix + "Digit histogram:   %s\n" % (str(stats['prediction_histogram']));
     
@@ -351,7 +359,7 @@ def test(model, dataset, dataset_data, label_index, parameters, max_length, base
         if (print_samples and not printed_samples):
             for i in range(nrSamples):
                 prefix = "# ";
-                whitespaceprefix = "".join([" " for k in range(parameters['lag'])]);
+                whitespaceprefix = "".join([" " for t in range(parameters['lag'])]);
                 if (parameters['dataset_type'] != 3):
                     print(prefix + "Intervention location: %d" % interventionLocations[0,i]);
                     whitespaceprefix = "";